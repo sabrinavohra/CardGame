83917a0632d9890c491458adbd10508a54de5c36
sabrinavohra <sabrina.vohra@menloschool.org>
2023-12-01 09:53:46 -0800

    Finished player class

--- src/Player.java
+++ src/Player.java
@@ -14,4 +14,28 @@ public class Player {
         this.name = name;
         this.hand = hand;
     }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Card> getHand() {
+        return this.hand;
+    }
+
+    public int getPoints() {
+        return this.points;
+    }
+
+    public void addPoints(int pts) {
+        points += pts;
+    }
+
+    public void addCard(Card crd) {
+        hand.add(crd);
+    }
+
+    public String toString() {
+        return name + " has " + points + " points \n" + name + "'s cards: " + hand;
+    }
 }
