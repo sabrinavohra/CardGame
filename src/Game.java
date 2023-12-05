9e2eaf139bb0ff4af646a4968d31fc8b0f1fc1b7
sabrinavohra <sabrina.vohra@menloschool.org>
2023-12-05 11:21:39 -0800

    Worked on placeCard method.

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,7 @@ public class Game {
     private Player p2;
     private ArrayList<Card> p2Hand;
     private Deck theDeck;
+    private ArrayList<Card> stack;
 
     public Game(Player player1, Player player2)
     {
@@ -33,7 +34,11 @@ public class Game {
     private void placeCard() {
         System.out.println("Enter the index of the card you want to play!");
         int index = p1.nextInt();
+        // create variables for points and suits so they can be compared
         if(p1.getHand().size() < index) {
-
+            if(p1.getHand(index) == stack.get(0)
+            stack.add(0, p1.get(index));
+            System.out.println("You placed the card: "stack.get(0));
+        }
     }
 }
