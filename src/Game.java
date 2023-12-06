0a3e788977c5bd921bea0486570fdbe51a49ad7c
sabrinavohra <sabrina.vohra@menloschool.org>
2023-12-05 22:40:40 -0800

    Finished draft of Game class

--- src/Game.java
+++ src/Game.java
@@ -29,7 +29,26 @@ public class Game {
         printInstructions();
         dealCards();
         System.out.println(theDeck.deal());
-        while()
+        while(hasWon() == false) {
+            System.out.println("The top card in the stack is: " + theDeck.get(0));
+            System.out.println("Would you like to place a card? Respond false if you have no card that matches the top card in the stack.");
+            if(currentPlayer == bPlayer1) {
+                boolean match = p1.nextBoolean();
+                if(match == false) {
+                    addCard();
+                }
+                placeCard();
+            }
+            else {
+                boolean match2 = p2.nextBoolean();
+                if(match2 == false) {
+                    addCard();
+                }
+                placeCard();
+            }
+            currentPlayer = !currentPlayer;
+        }
+        System.out.println("Great game!");
         //while(someone hasn't won / hasn't gotten rid of all their cards)
             //let player put down card that matches with the one on top of stack in at least one way (check) - placeCard();
             //let other play do the same thing - placeCard();
@@ -38,7 +57,6 @@ public class Game {
             //after each turn check if: they have won - hasWon() and if a player hasn't won then change currentPlayer to other player;
                 // outside of while loop: end game (!!) if they have and ask if they want to play again - playAgain();
                 //continue game if they haven't
-        //System.out.println(theDeck.deal());
     }
 
     private void dealCards() {
@@ -63,18 +81,38 @@ public class Game {
     private void placeCard() {
         System.out.println("Enter the index of the card you want to play!");
         int index;
-        do {
-            index = currentPlayer.nextInt();
+        if(currentPlayer == bPlayer1) {
+            do {
+                index = p1.nextInt();
+            } while(index > p1.getHand().size() || index < 0)
+            Card topOfStack = stack.get(0);
+            String stackCardRank = topOfStack.getRank();
+            String stackCardSuit = topOfStack.getSuit();
+            Card fromHand = p1Hand.get(index);
+            if(fromHand.getRank().equals(stackCardRank) || fromHand.getSuit().equals(stackCardSuit)) {
+                stack.add(0, p1.get(index));
+                System.out.println("You placed the card: " + stack.get(0));
+            }
         }
-        while(index > p1.getHand().size() || index < 0)
+        else {
+            do {
+                index = p2.nextInt();
+            } while(index > p2.getHand().size() || index < 0)
+        }
         Card topOfStack = stack.get(0);
         String stackCardRank = topOfStack.getRank();
         String stackCardSuit = topOfStack.getSuit();
-        Card fromHand = p1Hand.get(index);
+        Card fromHand = p2Hand.get(index);
         if(fromHand.getRank().equals(stackCardRank) || fromHand.getSuit().equals(stackCardSuit)) {
-            stack.add(0, p1.get(index));
+            stack.add(0, p2.get(index));
             System.out.println("You placed the card: " + stack.get(0));
         }
+    }
+    private void addCard() {
+        if(currentPlayer == bPlayer1) {
+            p1Hand.deal();
+        }
+        p2Hand.deal();
     }
     public boolean hasWon() {
         if(currentPlayer == bPlayer1) {
