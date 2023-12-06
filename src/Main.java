3466d9c2ee474c360e03893664b65fb72bbb4a5b
sabrinavohra <sabrina.vohra@menloschool.org>
2023-12-06 10:17:20 -0800

    Tried to run a game.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
 public class Main {
-
+    Player sabrina = new Player("sabrina");
+    Player other = new Player("other person");
+    Game newGame = new Game(sabrina, other);
+    newGame.playGame();
 }
