a4fce04d1104d0cf748c6fbb62ea9308a4fbee32
sabrinavohra <sabrina.vohra@menloschool.org>
2023-12-07 21:00:29 -0800

    Added comments.

--- src/Game.java
+++ src/Game.java
@@ -51,18 +51,17 @@ public class Game {
         printInstructions();
         // Deals cards to both players
         dealCards();
-        // Makes sure a Player hasn't won while running loop
+        // Makes sure a Player hasn't won in order to run loop
         while (hasWon() == false) {
             // Runs for Player 1
-            //if (currentPlayer == bPlayer1) {
+            if (currentPlayer == bPlayer1) {
                 // Prints and shows hand for user
-                System.out.println("Here's " + currentPlayerInfo.getName() + "'s hand: ");
+                System.out.println("Here's " + p1.getName() + "'s hand: ");
                 showHand();
                 // Prints top Card in stack
                 System.out.println("The top card in the stack is: " + stack.get(0));
-                // Asks user if they want to place a card - in case they don't have a playable card
-                System.out.println("Would you (" + currentPlayerInfo.getName() + ") like to place a card? Respond no " +
-                        "if you have no card that matches the top card in the stack.");
+                // Asks user if they want to place a Card - in case they don't have a playable card
+                System.out.println("Would you (" + p1.getName() + ") like to place a card? Respond no if you have no card that matches the top card in the stack.");
                 // Uses Scanner to receive user input
                 String response = input.nextLine();
                 // Checks user's response and either a) Places a Card or b) Adds a Card to their hand
@@ -71,56 +70,48 @@ public class Game {
                 } else {
                     addCard();
                 }
-                // Switches to Player 2
-                if(currentPlayer ==  bPlayer1) {
-                    currentPlayer = !currentPlayer;
-                    currentPlayerInfo = p2;
-                }
-                else {
-                    currentPlayer = !currentPlayer;
-                    currentPlayerInfo = p1;
-                }
+                // Switches Player 1 to Player 2
             }
-//            // Runs same process but for Player 2
-//            System.out.println("Here's " + p2.getName() + "'s hand: ");
-//            showHand();
-//            System.out.println("The top card in the stack is: " + stack.get(0));
-//            System.out.println("Would you (" + p2.getName() + ") like to place a card? Respond no if you have no card that matches the top card in the stack.");
-//            String response = input.nextLine();
-//            if(response.equalsIgnoreCase("yes")) {
-//                placeCard();
-//            }
-//            else {
-//                addCard();
-//            }
-//            currentPlayer = !currentPlayer;
-//        }
+            // Runs process for Player 2
+            System.out.println("Here's " + p2.getName() + "'s hand: ");
+            showHand();
+            System.out.println("The top card in the stack is: " + stack.get(0));
+            System.out.println("Would you (" + p2.getName() + ") like to place a card? Respond no if you have no card that matches the top card in the stack.");
+            String response = input.nextLine();
+            if(response.equalsIgnoreCase("yes")) {
+                placeCard();
+            }
+            else {
+                addCard();
+            }
+            currentPlayer = !currentPlayer;
+        }
         System.out.println("Great game!");
     }
-        //while(someone hasn't won / hasn't gotten rid of all their cards)
-            //let player put down card that matches with the one on top of stack in at least one way (check) - placeCard();
-            //let other play do the same thing - placeCard();
-            //if a player (has no cards that match the top one)
-                //add card to their hand until they can put down a card - addCard();
-            //after each turn check if: they have won - hasWon() and if a player hasn't won then change currentPlayer to other player;
-                // outside of while loop: end game (!!) if they have and ask if they want to play again - playAgain();
-                //continue game if they haven't
-
+    // Deals Cards to Players
     private void dealCards() {
+        // Shuffles Deck before dealing
         theDeck.shuffle();
+        // Deals hand to both Players
         for(int i = 0; i < 8; i++) {
             p1Hand.add(theDeck.deal());
             p2Hand.add(theDeck.deal());
         }
+        // Flips over first card
         stack.add(theDeck.deal());
     }
+    // Shows hand for given Player
     private void showHand() {
+        // Shows Player 1's hand
         if(currentPlayer == bPlayer1) {
+            // Prints each Card in hand individually
             for(int i = 0; i < p1Hand.size(); i++) {
                 System.out.print(p1Hand.get(i) + ", ");
             }
+            // Prints new line
             System.out.println();
         }
+        // Does same process for Player 2
         else {
             for(int i = 0; i < p2Hand.size(); i++) {
                 System.out.print(p2Hand.get(i) + ", ");
@@ -128,24 +119,36 @@ public class Game {
             System.out.println();
         }
     }
+    // Places Card on stack
     private void placeCard() {
+        // Creates Scanner to get user input later
         Scanner response = new Scanner(System.in);
         System.out.println("Enter the index of the card you want to play!");
         int index;
         if(currentPlayer == bPlayer1) {
+            // Makes sure user inputs valid response
             do {
                 index = response.nextInt();
             } while(index >= p1Hand.size() || index < 0);
+            // Creates variable for the Card on the top of the stack
             Card topOfStack = stack.get(0);
+            // Gets rank of top Card
             String stackCardRank = topOfStack.getRank();
+            // Gets suit of top Card
             String stackCardSuit = topOfStack.getSuit();
+            // Gets Card from user's input
             Card fromHand = p1Hand.get(index);
+            // Checks to make sure Card is valid by comparing it to top Card on stack
             if(fromHand.getRank().equals(stackCardRank) || fromHand.getSuit().equals(stackCardSuit)) {
+                // Adds user's Card to top of stack
                 stack.add(0, p1Hand.get(index));
-                //System.out.println("You placed the card " + fromHand);
+                // Tells user which Card they placed
+                System.out.println("You placed the card " + fromHand);
+                // Removes their Card from their hand
                 p1Hand.remove(fromHand);
             }
         }
+        // Repeats process for Player 2
         else {
             do {
                 index = response.nextInt();
@@ -157,26 +160,32 @@ public class Game {
         Card fromHand = p2Hand.get(index);
         if(fromHand.getRank().equals(stackCardRank) || fromHand.getSuit().equals(stackCardSuit)) {
             stack.add(0, p2Hand.get(index));
-            //System.out.println("You placed the card " + fromHand);
+            System.out.println("You placed the card " + fromHand);
             p2Hand.remove(fromHand);
-            //System.out.println("You placed the card: " + stack.get(0));
         }
     }
+    // Adds Card to Player's hand
     private void addCard() {
+        // Adds card from Deck to Player 1's hand
         if(currentPlayer == bPlayer1) {
             p1Hand.add(theDeck.deal());
         }
+        // Adds card from Deck to Player 2's hand
         p2Hand.add(theDeck.deal());
     }
+
+    // Checks to see if user has won
     private boolean hasWon() {
+        // Checks for Player 1
         if(currentPlayer == bPlayer1) {
+            // Checks if the size of Player 1's hand is 0 - meaning they have no Cards
             if(p1Hand.size() == 0) {
                 return true;
             }
-            else {
-                return false;
-            }
+            // Returns false if Player 1's hand has more than 1 Card
+            return false;
         }
+        // Repeats process for Player 2
         else {
             if(p2Hand.size() == 0) {
                 return true;
@@ -186,10 +195,14 @@ public class Game {
             }
         }
     }
+    // Main method
     public static void main(String[] args) {
+        // Initializes two Players
         Player sabrina = new Player("sabrina");
         Player other = new Player("other player");
+        // Creates new Game using Constructor with two Players
         Game newGame = new Game(sabrina, other);
+        // Plays a game!
         newGame.playGame();
     }
 }
